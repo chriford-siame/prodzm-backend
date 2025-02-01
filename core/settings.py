e18953d76d44b7dbf1d3595a2670e7ba2601248f
chriford-siame <chrifordsiame@gmail.com>
2025-02-01 20:45:35 +0200

    update installed apps

--- core/settings.py
+++ core/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'prodzm',
     
-    "drf_yasg2",
+    "drf_yasg",
     "rest_framework",
     "rest_framework_simplejwt",
     "rolepermissions",
