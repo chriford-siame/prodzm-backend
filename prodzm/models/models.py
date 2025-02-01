6a334587bed2a81afe22e9a0aa05070cab93168b
chriford-siame <chrifordsiame@gmail.com>
2025-02-01 21:21:41 +0200

    create category model

--- prodzm/models/models.py
+++ prodzm/models/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    """
+    Represents a product category (e.g., Electronics, Clothing) that groups similar products together.
+    """
+    name = models.CharField(max_length=255, help_text="The name of the category.")
+    description = models.TextField(blank=True, help_text="Optional description of the category.")
+
+    def __str__(self):
+        return self.name
+
