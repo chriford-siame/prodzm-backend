73097ca8b0abba6674ae4bd912562f9a878da15a
chriford-siame <chrifordsiame@gmail.com>
2025-02-02 00:08:13 +0200

    enable image view from the admin pannel

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from django.urls import path, re_path, include
-
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import (
+    path, 
+    re_path, 
+    include
+)
 from rest_framework import permissions
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
@@ -36,3 +41,6 @@ urlpatterns = [
     path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
     
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
