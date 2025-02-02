17a436df0f23410f9716262c60fc4704d4c84a2f
chriford-siame <chrifordsiame@gmail.com>
2025-02-02 20:12:37 +0200

    register viewsets

--- prodzm/routers.py
+++ prodzm/routers.py
@@ -1,14 +1,20 @@
 from rest_framework.routers import DefaultRouter
 
-# from prodzm.restful_api.viewsets  import (
-#     AnswerViewSet,
-#     QuestionViewSet,
-#     ReportViewSet,
-#     UserProfileViewSet,
-# )
+from prodzm.viewsets import (
+    ProductViewSet, ProductImageViewSet, CategoryViewSet, ReviewViewSet,
+    CustomerViewSet, OrderViewSet, OrderItemViewSet, ShippingViewSet
+)
 
+# Create a router and register the ViewSets
 router = DefaultRouter()
-# router.register(r'users', UserProfileViewSet, basename='user')
+router.register(r'products', ProductViewSet)
+router.register(r'product-images', ProductImageViewSet)
+router.register(r'categories', CategoryViewSet)
+router.register(r'reviews', ReviewViewSet)
+router.register(r'customers', CustomerViewSet)
+router.register(r'orders', OrderViewSet)
+router.register(r'order-items', OrderItemViewSet)
+router.register(r'shipping', ShippingViewSet)
 
 app_name = 'prodzm'
 urlpatterns = router.urls
