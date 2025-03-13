fd67c36da1b3c53c017208d379631867e1493efb
chriford-siame <chrifordsiame@gmail.com>
2025-03-13 13:54:55 +0200

    Enable search fuctionality

--- prodzm/viewsets/viewsets.py
+++ prodzm/viewsets/viewsets.py
@@ -9,6 +9,10 @@ from prodzm.models import (
     OrderItem, 
     Shipping
 )
+from django.db.models import Q
+
+from rest_framework.response import Response
+from rest_framework.decorators import action
 from prodzm.serializers import (
     ProductSerializer, 
     ProductImageSerializer, 
@@ -27,6 +31,44 @@ class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    
+    @action(detail=False, methods=['get'])
+    def search(self, request):
+        """
+        Custom API endpoint to filter products by category.\n
+        Example: /api/products/search/?category=Electronics
+        """
+        queryset = Product.objects.all()
+        
+        # Filtering by category (if provided)
+        category = request.query_params.get('category')
+        if category:
+            queryset = queryset.filter(category__name=category)
+
+        # Filtering by number of orders (if provided)
+        orders = request.query_params.get('orders')
+        if orders:
+            queryset = queryset.filter(orders__gte=orders)
+
+        # Filtering by creation date (if provided)
+        date = request.query_params.get('date')
+        if date:
+            queryset = queryset.filter(created_at__date=date)
+
+        # Filtering by price range (if provided)
+        price = request.query_params.get('price')
+        if price:
+            queryset = queryset.filter(price__lte=price)
+
+        # Search query across multiple fields
+        qs = request.query_params.get('search')
+        if qs:
+            queryset = queryset.filter(
+                Q(name__icontains=qs) | Q(description__icontains=qs)
+            )
+
+        serializer = ProductSerializer(queryset, many=True)
+        return Response(serializer.data)
 
 class ProductImageViewSet(viewsets.ModelViewSet):
     """
