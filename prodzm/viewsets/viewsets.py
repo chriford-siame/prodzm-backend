4eba579b3f234703671d82b9afe4af2d59293357
chriford-siame <chrifordsiame@gmail.com>
2025-03-14 12:11:29 +0200

    modify qs

--- prodzm/viewsets/viewsets.py
+++ prodzm/viewsets/viewsets.py
@@ -27,6 +27,47 @@ class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+<<<<<<< Updated upstream
+=======
+    
+    @action(detail=False, methods=['get'])
+    def search(self, request):
+        """
+        Custom API endpoint to filter products by category.\n
+        Example: /api/products/search/?category=Electronics
+        """
+        queryset = Product.objects.all()
+        
+        # Filtering by category (if provided)
+        category = request.query_params.get('category')
+        if category:
+            queryset = queryset.filter(category__name=category)
+
+        # Filtering by number of orders (if provided)
+        orders = request.query_params.get('orders')
+        if orders and orders != 0:
+            queryset = queryset.filter(orders__gte=orders)
+
+        # Filtering by creation date (if provided)
+        date = request.query_params.get('date')
+        if date:
+            queryset = queryset.filter(created_at__date=date)
+
+        # Filtering by price range (if provided)
+        price = request.query_params.get('price')
+        if price and price != 0:
+            queryset = queryset.filter(price__lte=price)
+
+        # Search query across multiple fields
+        qs = request.query_params.get('qs')
+        if qs:
+            queryset = queryset.filter(
+                Q(name__icontains=qs) | Q(description__icontains=qs)
+            )
+
+        serializer = ProductSerializer(queryset, many=True)
+        return Response(serializer.data)
+>>>>>>> Stashed changes
 
 class ProductImageViewSet(viewsets.ModelViewSet):
     """
